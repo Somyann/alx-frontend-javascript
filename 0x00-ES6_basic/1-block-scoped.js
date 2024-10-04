31c3bcd981003524dbc49ba73773e83bfc1abd70
Somyann <somiaprecious91@gmail.com>
2024-10-04 19:12:22 +0100

    arrays

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,11 @@
 export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
   if (trueOrFalse) {
     const task = false;
     const task2 = true;
-
-    return [task, task2];
   }
-  return [trueOrFalse];
+  
+  return [task, task2];
 }

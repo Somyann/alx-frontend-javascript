a3b1123f55ba0d37083ef60e812772ed9de34dbc
Somyann <somiaprecious91@gmail.com>
2024-10-08 15:00:48 +0100

    more mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
-export default function getListStudents() {
-  return [
-    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
-    { firstName: 'James', id: 2, location: 'Columbia' },
-    { firstName: 'Serena', id: 5, location: 'San Francisco' },
-  ];
+export default function getListStudents(arr) {
+  if (!Array.isArray(arr)) {
+  return [];
+  }
+  const ids = arr.map((x) => x.id);
+  return ids;
 }

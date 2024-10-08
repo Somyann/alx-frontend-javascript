cc9687396fe107c7f2910942eacc41392974ff7e
Somyann <somiaprecious91@gmail.com>
2024-10-08 22:15:02 +0100

    position

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -9,5 +9,5 @@ export default function createInt8TypedArray(length, position, value) {
   const int8Array = new Int8Array(buffer);
   int8Array[position] = value;
 
-  return int8Array;
+  return buffer;
 }

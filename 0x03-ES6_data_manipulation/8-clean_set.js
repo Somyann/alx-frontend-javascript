09088c61f9ba8c22104920e5a2892b45c6f6eb78
Somyann <somiaprecious91@gmail.com>
2024-10-09 10:28:28 +0100

    jovi apparte

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,16 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') {
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
 
-  const result = [];
-
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
     }
   }
-  return result.join('-');
+  return parts.join('-');
 }

562427aae6495db72c612169bcfe7cc843ade6b0
Somyann <somiaprecious91@gmail.com>
2024-10-11 18:20:16 +0100

    studentlist

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,13 +1,12 @@
 interface Teacher {
   readonly firstName: string;   
-  readonly lastName: string;     // Modifiable only at initialization
-  fullTimeEmployee: boolean;     // Must always be defined
-  yearsOfExperience?: number;    // Optional attribute
-  location: string;              // Must always be defined
-  [key: string]: any;            
+  readonly lastName: string;     
+  fullTimeEmployee: boolean;     
+  yearsOfExperience?: number;    
+  location: string;           
+  [key: string]: any;           
 }
 
-// Example of creating a Teacher object
 const teacher: Teacher = {
   firstName: 'Somia',
   lastName: 'Ann',
@@ -15,4 +14,38 @@ const teacher: Teacher = {
   location: 'Nigeria',
   contract: false,   // Additional property
 };
+interface Directors extends Teacher {
+  numberOfReports: Number,
+}
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;  
+}
+const printTeacher: printTeacherFunction = (firstName: string,lastName: string): string => '${firstName.charAt(0)}.${lastName}'
 
+interface classInterface{
+  workOnHomework(): string;
+  displayName(): string;
+}
+class StudentClass implements classInterface{
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName;
+    this.lastName;
+  }
+
+  workOnHomework():string{
+    return "Currently working";
+  }
+  displayName():string{
+    return this.firstName;
+  }
+}
+interface StudentConstructor {
+  (firstName: string, lastName: string): classInterface;
+}
+
+const student = new StudentClass("Djo", "djo");
+console.log(student.displayName())
+console.log(student.workOnHomework())
